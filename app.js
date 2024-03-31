9341b31c4e8f8a41ecc01a4b97d2f983c112893d
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-31 16:59:29 -0700

    implement oauth using passport and google strategy

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars')
-const connectDB = require('./config/db');
+const passport = require('passport')
+const session = require('express-session')
 const path = require('path')
+const connectDB = require('./config/db');
 
-//Load config
+// Load config
 dotenv.config({path: './config/config.env'});
+
+// Passport config
+require('./config/passport')(passport)
+
 connectDB();
 
 const app = express();
+
 // Logging
 if(process.env.NODE_ENV == 'development'){
     app.use(morgan('dev'))
@@ -20,11 +27,23 @@ app.engine('.hbs', exphbs.engine({
     extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-//Static Folder
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
 app.use(express.static(path.join(__dirname,'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000;
 

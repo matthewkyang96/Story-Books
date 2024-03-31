61ff0676bc04689a0e295616c309bd26b2cdeb22
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-31 16:09:48 -0700

    set up additional frontend framework

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db');
+const path = require('path')
 
 //Load config
 dotenv.config({path: './config/config.env'});
 connectDB();
 
 const app = express();
-
+// Logging
 if(process.env.NODE_ENV == 'development'){
     app.use(morgan('dev'))
 }
@@ -18,6 +19,12 @@ app.engine('.hbs', exphbs.engine({
     defaultLayout: 'main',
     extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Static Folder
+app.use(express.static(path.join(__dirname,'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000;
 

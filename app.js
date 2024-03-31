496d3f8d5105ecc6662e87b698dbdbb5336f27a6
Matt Y <49535190+matthewkyang96@users.noreply.github.com>
2024-03-31 15:54:20 -0700

    add handlebars as templating engine

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db');
-
 
 //Load config
 dotenv.config({path: './config/config.env'});
 connectDB();
 
 const app = express();
+
+if(process.env.NODE_ENV == 'development'){
+    app.use(morgan('dev'))
+}
+// HandleBars
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
 
